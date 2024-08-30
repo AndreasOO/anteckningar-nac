f5d4cb9efc59f2e75e845cc15c3fc6da859a00f4
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-08-30 22:37:52 +0200

    Attempted answering question 4.5
    Created methods to calculate radix.
    Calculated difference with abs.
    For smaller numbers, i used parseInt with substring starting with exponent E of scientific notation, but it results in problems when decimalnumer is large enough to not have scientific notation.

--- src/Lektioner.java
+++ src/Lektioner.java
@@ -19,8 +19,47 @@ public class Lektioner {
 //        GymTicketCalculator gtc = new GymTicketCalculator();
 //        gtc.execute();
 
-        MathTestGrader mathTestGrader = new MathTestGrader();
-        mathTestGrader.execute();
+//        MathTestGrader mathTestGrader = new MathTestGrader();
+//        mathTestGrader.execute();
+
+        // Beräkna radix med vinkel
+        double angle = 60;
+        double radix = angle*(Math.PI/180.0);
+//        System.out.println(radix);
+
+        // problem, parseInt metoden nere får numberformat exception pga inte hittar E. Lös på bättre sätt.
+        double a = 9.99;
+        double b = 9.98;
+        double c = Math.sqrt(Math.pow(a,2)+Math.pow(b,2) - 2*(a*b)*Math.cos(radix));
+
+        double diffAB = Math.abs(a-b);
+        double diffAC = Math.abs(a-c);
+        double diffBC = Math.abs(b-c);
+
+        String diffStringAC = String.valueOf(diffAC);
+        int exponentAC = Integer.parseInt(diffStringAC.substring(diffStringAC.indexOf("E")+1));
+
+        String diffStringAB = String.valueOf(diffAB);
+        int exponentAB = diffAB == 0.0 ? 1 : Integer.parseInt(diffStringAB.substring(diffStringAB.indexOf("E")+1));
+
+        String diffStringBC = String.valueOf(diffBC);
+        int exponentBC = diffBC == 0.0 ? 1 : Integer.parseInt(diffStringBC.substring(diffStringBC.indexOf("E")+1));
+
+        System.out.println(exponentAB);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 
