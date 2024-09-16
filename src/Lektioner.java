6da7d29aabe302e2f4e659ba74deff1fc379d91a
Andreas Ohlander <andreas.ohlander@gmail.com>
2024-09-16 09:21:46 +0200

    Added notes on IntSummaryStatistics

--- src/Lektioner.java
+++ src/Lektioner.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 public class Lektioner {
@@ -12,9 +13,24 @@ public class Lektioner {
     private static int a;
     public static void main(String[] args) {
 
+        int[] arr = {1,1,2,2,3,3,4,4};
 
-    int n = 5;
-    int[] arr = Arrays.stream(String.valueOf(n).split("")).mapToInt(Integer::parseInt).toArray();
+        int sum = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+
+
+        for (int ele : arr) {
+            map.put(ele, map.get(ele) == null ? 1 : map.get(ele)+1);
+        }
+
+//        for (Integer ele : map.keySet()) {
+//            System.out.println(ele + "  " + map.get(ele));
+//
+//        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            System.out.println(entry.getKey() + "  " + entry.getValue());
+        }
+
 
     }
 
